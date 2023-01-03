cfff0ebd72825eba5f6764a0aec194fd49c1ac83
doksyilji <chatdoks@gmail.com>
2023-01-03 23:27:33 +0000

    6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 for i in range(10):
-  for j in range(10):
-    if j <= i:
-        continue
-    if i != j:
-        if i == 0 and j == 1:
-            print("{:d}{:d}".format(i, j), end="")
-        else:
-            print(", {:d}{:d}".format(i, j), end="")
+    for j in range(10):
+        if j <= i:
+            continue
+        if i != j:
+            if i == 0 and j == 1:
+                print("{:d}{:d}".format(i, j), end="")
+            else:
+                print(", {:d}{:d}".format(i, j), end="")
